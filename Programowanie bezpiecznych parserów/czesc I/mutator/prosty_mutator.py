2128f2e32ce77bcd43a8a7e3448f040f15119a10
JokerTux <patrick.slomian90@gmail.com>
2023-10-14 15:36:13 +0200

    cd.

---
+++ Programowanie bezpiecznych parserów/czesc I/mutator/prosty_mutator.py
@@ -0,0 +1,34 @@
+import random
+from os import urandom
+import sys
+
+
+def read_bytes(f, n):  
+    if f != n:
+        raise Exception('Bytes do not match !!!')
+
+
+if len(sys.argv) < 2:
+	sys.exit('usage: mut.py <fname>')
+
+infile = sys.argv[1]
+outfile = sys.argv[1] + '.mut'
+
+f = bytearray(open(infile, 'rb').read())
+len_f = len(f)
+for i in range(0, len(f), 256):
+	#f.append(1)
+	random_offset = random.randint(0, 255)
+	random_sz_bytes = random.randrange(1, 8, 1)
+	#print(random_offset, random_sz_bytes)
+	for j in range(i + random_offset, i + random_offset + random_sz_bytes):
+		if j < len(f):
+			f[j] = random.randint(0, 255) 
+			#print(j)
+
+read_bytes(len_f, len(f))
+open(outfile, 'wb').write(f)	
+
+
+# print(random_offset, random_nr_bytes, random_bytes_values)
+
