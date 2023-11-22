98a4e073297e0863f5c2f432f2ad53bc558ddb54
JokerTux <patrick.slomian90@gmail.com>
2023-11-22 22:19:28 +0100

    cd.

--- Programowanie bezpiecznych parserów/czesc II/praca_domowa/parser_bmp_rle.py
+++ Programowanie bezpiecznych parserów/czesc II/praca_domowa/parser_bmp_rle.py
@@ -3,7 +3,57 @@ import sys
 
 
 def verification(f):
-	pass
+	file_sz = len(f.read())
+
+	f.seek(0)
+	header_info = f.read(2)
+	if header_info.hex() != '424d':
+		raise Exception('Unsupported file type')
+	
+	f.seek(2)
+	file_size = f.read(4)
+	file_size = struct.unpack('<I', file_size)[0]
+	if file_size != file_sz:
+		print(file_size, file_sz)
+		raise Exception('Declared bytes size is not equal to the actual file size')	
+
+	f.seek(14)
+	bmp_info_header = f.read(4)
+	bmp_info_header = struct.unpack('<I', bmp_info_header)[0]
+	if bmp_info_header != 40:
+		raise Exception('BITMAPINFOHEADER != 40')	
+
+	f.seek(28)
+	color_depth = f.read(2)
+	color_depth = struct.unpack('<H', color_depth)[0]
+	if color_depth != 8:
+		raise Exception('Unsupported .bmp type, "Color" was not declared [palette]')		
+		
+	f.seek(30)
+	compression_method= f.read(4)
+	compression_method = struct.unpack('<I', compression_method)[0]
+	if compression_method != 1:
+		raise Exception('Wrong compression method')
+
+	f.seek(46)
+	declared_colors_bin = f.read(4)
+	declared_colors_bin = struct.unpack('<I', declared_colors_bin)[0]
+	_check = 54 + (declared_colors_bin * 4) - 1
+	f.seek(_check)
+	byte_check = f.read(1)
+	byte_check = struct.unpack('<B', byte_check)[0]
+	if byte_check != 0:
+		raise Exception('The color plaette is to long/short ')
+		
+	_check2 = _check + 1
+	f.seek(_check2)
+	data_sz = len(f.read())
+	f.seek(34)
+	data_lenght = f.read(4)
+	data_lenght = struct.unpack('<I', data_lenght)[0]
+	if (file_size - data_lenght) != _check2:
+		raise Exception('Offset error')
+
 
 
 def resolution(f):
@@ -63,7 +113,7 @@ def get_rle(f):
 		rle = f.read(img_sz)
 		for i in rle:
 			rle_bytes_list.append(i)
-		print(f'Declared bmp bytes = {rle}')
+		# print(f'Declared bmp bytes = {rle}')
 	else:
 		raise Exception('Error,this file has no data')
 
@@ -81,20 +131,15 @@ def export_file(width, height, rle_bytes, color_bytes_rgb):
 			if a == 0 and b == 0 or a == 0 and b == 1:
 				continue	
 			indexes.append(b)
-	print(indexes)
+	#print(indexes)
 	raw_colors = []
 	for bytes_rle in indexes:
 		raw_color = color_bytes_rgb[bytes_rle]
 		raw_colors.append(raw_color)
 	
-
-	# abc = 0
-	# for ab in raw_colors:
-	# 	abc += 1
-	# 	print(abc, ab.hex())
 	buf = b''	
 	end_of_bytes_list = len(raw_colors)	* height
-	print(end_of_bytes_list)
+	# print(end_of_bytes_list)
 	for scanline in range(end_of_bytes_list, 0, -(width)):
 		end_of_bytes_list -= width
 		if scanline <= width:
